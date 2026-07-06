8883fde1799aa277022502661698ce4903c4d6d1
syyaaxxlocker <chocomoorebr@gmail.com>
2026-07-06 09:31:34 +0000

    Fix fucking xdg-desktop-portal-wlr run! Stream in Discord is real now

--- .config/scripts/dwl/autostart.sh
+++ .config/scripts/dwl/autostart.sh
@@ -3,9 +3,10 @@
 pkill mako 2>/dev/null
 mako &
 
-export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
-
 sleep 1
+
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
 
 wl-paste --type text --watch cliphist store &
 wl-paste --type image --watch cliphist store &
---
+++ start_dwl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export XDG_CURRENT_DESKTOP=dwl
+export XDG_SESSION_DESKTOP=dwl
+export XDG_SESSION_TYPE=wayland
+
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+    eval "$(dbus-launch --sh-syntax)"
+fi
+
+systemctl --user import-environment XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE
+
+dbus-run-session /usr/local/bin/slstatus -s | /usr/local/bin/dwl -s '${HOME}/.config/scripts/dwl/autostart.sh <&-'
